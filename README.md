5ec55979207aff29dde637aeddf6aa85ea74ac5b
w0xt3r <gerard7@hotmail.es>
2017-06-06 12:13:11 +0200

    start project

---
+++ README.md
@@ -0,0 +1,2 @@
+# boilerplate-webpack-react-ts
+ Boilerplate for react and typescript bundled with webpack and hmr.
---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+
+
+export default class App extends React.Component<undefined, undefined> {
+
+    public render(): JSX.Element {
+        return <div>
+            <h1>Hello World!</h1>
+            <p>Full boilerplate for react and typescript with webpack2 and hot module replacement</p>
+        </div>;
+    }
+
+}
